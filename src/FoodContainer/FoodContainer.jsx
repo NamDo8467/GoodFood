498342759c80e7a0dbec3db9466d55b820eac02c
NamDo8467 <huynhnamdo@gmail.com>
2021-09-07 10:38:27 +0700

    add skeleton effect

--- src/FoodContainer/FoodContainer.jsx
+++ src/FoodContainer/FoodContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import "./FoodContainer.css";
 const FoodContext = React.createContext("No food");
 const MealTypeContext = React.createContext("Unknown");
@@ -18,6 +18,13 @@ function FoodContainer(props) {
   );
 }
 
+// const delay = (milliseconds) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve()
+//     },milliseconds)
+//   })
+// }
 const Meal = (props) => {
   return props.children;
 };
@@ -25,6 +32,7 @@ const Meal = (props) => {
 const Food = () => {
   const foodInfo = useContext(FoodContext);
   const mealType = useContext(MealTypeContext);
+  const [skeleton, setSkeleton] = useState(true);
   let names = [];
   let images = [];
 
@@ -38,39 +46,62 @@ const Food = () => {
     for (let i = 0; i < allImages.length; i++) {
       images = images.concat(allImages[i]);
     }
-    
   } else {
     const food = foodInfo.filter((food) => {
       return food.type === mealType;
     });
     names = food[0].names;
     images = food[0].images;
-    // console.log(images);
   }
-  return (
-    <div className="food-container">
-      {names.map((name) => (
-        <div key={name} className="food">
-          <img
-            className="food-image"
-            src={images[names.indexOf(name)].default}
-            alt={mealType}
-            height="150"
-            width="170"
-          />
-          <div className="food-information">
-            <h3 className="name">{name}</h3>
-            <h3 className="price">Price: $15.99</h3>
-            <div className="line"></div>
-            <p className="information">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam id
-              nobis voluptas, harum neque ipsam esse quaerat molestiae incidunt.
-            </p>
+  setTimeout(() => {
+    setSkeleton(false);
+  }, 2500);
+
+  if (skeleton) {
+    return (
+      <div className="food-container">
+        {names.map((name) => (
+          <div className="food">
+            <div className="food-image-skeleton"></div>
+            <div className="food-information food-information-skeleton">
+              <div className="name name-skeleton"></div>
+              <div className="price price-skeleton"></div>
+              <div className="information information-skeleton"></div>
+              <div className="information information-skeleton"></div>
+              <div className="information information-skeleton"></div>
+              <div className="information information-skeleton"></div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="food-container">
+        {names.map((name) => (
+          <div key={name} className="food">
+            <img
+              className="food-image"
+              src={images[names.indexOf(name)].default}
+              alt={mealType}
+              height="150"
+              width="170"
+            />
+            <div className="food-information">
+              <h3 className="name">{name}</h3>
+              <h3 className="price">Price: $15.99</h3>
+              <div className="line"></div>
+              <p className="information">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+                id nobis voluptas, harum neque ipsam esse quaerat molestiae
+                incidunt.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default FoodContainer;
