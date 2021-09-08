4a3a9aa5c1e1f98decb5faf29c0b9530fd400805
NamDo8467 <huynhnamdo@gmail.com>
2021-09-08 11:15:34 +0700

    add contexts to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Title/Title";
 import Category from "./Category/Category.jsx";
+import food from "./images/index";
+import FoodContainer from "./FoodContainer/FoodContainer";
 
+export const FoodContext = React.createContext("No food");
+export const MealTypeContext = React.createContext("all");
 
 function App() {
+  const [mealType, setMealType] = useState("all");
+
   return (
-    <div>
-      <Title />
-      <Category />
-    </div>
+    <FoodContext.Provider value={food}>
+      <MealTypeContext.Provider value={mealType}>
+        <Title />
+        <Category setTypeFunction={setMealType} />
+        <FoodContainer />
+      </MealTypeContext.Provider>
+    </FoodContext.Provider>
   );
 }
 
