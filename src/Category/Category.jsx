3200e308a0463948bf220bcc04700c5522813297
NamDo8467 <huynhnamdo@gmail.com>
2021-09-08 11:13:48 +0700

    refactor code

--- src/Category/Category.jsx
+++ src/Category/Category.jsx
@@ -1,16 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Category.css";
 import "../FoodContainer/FoodContainer";
-import FoodContainer from "../FoodContainer/FoodContainer";
-function Category() {
-  const [type, setType] = useState('all')
+
+function Category(props) {
+  const setType = props.setTypeFunction
   const handleCategoryChange = (e) => {
-      setType(e.target.dataset.type)
-  };
+    setType(e.target.dataset.type)
+  }
   return (
     <div>
       <div className="category-container">
-        <ul className="category food-category">
+        <ul className="category-list">
           <li data-type="all"      onClick={(e)=>{handleCategoryChange(e)}}>All</li>
           <li data-type="breakfast"    onClick={(e)=>{handleCategoryChange(e)}}>Breakfast</li>
           <li data-type="lunch"onClick={(e)=>{handleCategoryChange(e)}}>Lunch</li>
@@ -18,7 +18,6 @@ function Category() {
           <li data-type="drink"   onClick={(e)=>{handleCategoryChange(e)}}>Drinks</li>
         </ul>
       </div>
-      <FoodContainer type={type} />
     </div>
   );
 }
